c8c5d801d8b761a2c897c0ca705a810c7c6e242e
FabioSzabo <fabioszabo14@gmail.com>
2023-02-04 11:56:29 -0300

    agregamos link de descarga

--- extreact/src/Componentes/campus.js
+++ extreact/src/Componentes/campus.js
@@ -1,13 +1,16 @@
 import Card from '../Componentes/card';
+import mate from '../imagenes proyecto/mate.webp'
+import ing from '../imagenes proyecto/ingles.jpg'
+import inf from '../imagenes proyecto/informatica.png'
+
 import '../css/campus.css';
-import mat from '../imagenes proyecto/mtematicas.png'
 function Campus() {
     return(
         <section>
         <div className='containerniveles'>
-<Card className="nivel bi bi-calculator-fill" src={mat} name="Matematicas" text="La matematica me hace dolor La enseñanza de la Matemática tiene como propósito fundamental desarrollar la capacidad para pensar, razonar, comunicar, aplicar y valorar las relaciones entre las ideas y los fenómenos reales.la cabeza"/>
-<Card className="nivel" name="Ingles" text="Cuando se comienza a aprender inglés, siempre hay quien da su opinión sobre cómo hacerlo. Algunos dicen que se debe estudiar mucho tiempo, otros que en poco tiempo se logra, que se debe tomar un curso avanzado o que empezar de cero es lo mejor. Inglés tambien"/>
-<Card className="nivel" name="Informatica" text="Automatizar todo tipo de procesos y sistemas para gestionar datos: Con lo que conseguimos aumentar enormemente la productividad, la velocidad en la creación y manejo de tareas y además evitamos muchos errores humanos"/>       
+<Card image={mate} className="nivel bi bi-calculator-fill" name="Matematicas" text="La matematica me hace dolor La enseñanza de la Matemática tiene como propósito fundamental desarrollar la capacidad para pensar, razonar, comunicar, aplicar y valorar las relaciones entre las ideas y los fenómenos reales.la cabeza"/>
+<Card image={ing} className="nivel" name="Ingles" text="Cuando se comienza a aprender inglés, siempre hay quien da su opinión sobre cómo hacerlo. Algunos dicen que se debe estudiar mucho tiempo, otros que en poco tiempo se logra, que se debe tomar un curso avanzado o que empezar de cero es lo mejor. Inglés tambien"/>
+<Card image={inf} className="nivel" name="Informatica" text="Automatizar todo tipo de procesos y sistemas para gestionar datos: Con lo que conseguimos aumentar enormemente la productividad, la velocidad en la creación y manejo de tareas y además evitamos muchos errores humanos"/>       
 </div>  
         </section>
     )
--- extreact/src/Componentes/card.js
+++ extreact/src/Componentes/card.js
@@ -1,23 +1,16 @@
 import '../css/campus.css'
 import pdf from '../pdf/matematicas.pdf'
-import ScrollLink from 'react-scroll/modules/mixins/scroll-link'
 function Card(props){
     return(
 <div className="card">
-  <img className="card-img-top"></img>
+  <img className="card-img-top" src={props.image}></img>
   <div className="card-body">
     <h5 className="card-title">{props.name}</h5>
     <p className="card-text">{props.text}</p>
-    <div>
-  <ScrollLink to="mywork" smooth={true}>
-    <button className={`btn`}>
-      <a href={pdf} target="_blank" rel="noopener noreferrer" download="matematicas.pdf"> 
-      Descarga tu trabajo
-      </a>
-    </button>
-  </ScrollLink>
-</div>
   </div>
+  <button class="btn btn-primary" type="button">
+      <a className='enlace' href={pdf} download="matematicas.pdf">Descarga tu trabajo</a>
+      </button>
 </div>
     )
 }
